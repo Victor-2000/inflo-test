af0642f74addfa2140d0afd575ce0b264887fb76
Victor <17victor.c@gmail.com>
2023-11-02 19:56:29 +0000

    Populated the dummy data with random birthdates

--- UserManagement.Data/DataContext.cs
+++ UserManagement.Data/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using UserManagement.Models;
 
@@ -11,20 +12,29 @@ public class DataContext : DbContext, IDataContext
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseInMemoryDatabase("UserManagement.Data.DataContext");
 
+    private DateTime GenerateRandomBirthday()
+    {
+        Random rnd = new Random();
+        DateTime start = new DateTime(1970, 1, 1);
+        int range = (DateTime.Today - start).Days;
+
+        return start.AddDays(rnd.Next(range));
+    }
+
     protected override void OnModelCreating(ModelBuilder model)
         => model.Entity<User>().HasData(new[]
         {
-            new User { Id = 1, Forename = "Peter", Surname = "Loew", Email = "ploew@example.com", IsActive = true },
-            new User { Id = 2, Forename = "Benjamin Franklin", Surname = "Gates", Email = "bfgates@example.com", IsActive = true },
-            new User { Id = 3, Forename = "Castor", Surname = "Troy", Email = "ctroy@example.com", IsActive = false },
-            new User { Id = 4, Forename = "Memphis", Surname = "Raines", Email = "mraines@example.com", IsActive = true },
-            new User { Id = 5, Forename = "Stanley", Surname = "Goodspeed", Email = "sgodspeed@example.com", IsActive = true },
-            new User { Id = 6, Forename = "H.I.", Surname = "McDunnough", Email = "himcdunnough@example.com", IsActive = true },
-            new User { Id = 7, Forename = "Cameron", Surname = "Poe", Email = "cpoe@example.com", IsActive = false },
-            new User { Id = 8, Forename = "Edward", Surname = "Malus", Email = "emalus@example.com", IsActive = false },
-            new User { Id = 9, Forename = "Damon", Surname = "Macready", Email = "dmacready@example.com", IsActive = false },
-            new User { Id = 10, Forename = "Johnny", Surname = "Blaze", Email = "jblaze@example.com", IsActive = true },
-            new User { Id = 11, Forename = "Robin", Surname = "Feld", Email = "rfeld@example.com", IsActive = true },
+            new User { Id = 1, Forename = "Peter", Surname = "Loew", DateOfBirth = GenerateRandomBirthday(), Email = "ploew@example.com", IsActive = true },
+            new User { Id = 2, Forename = "Benjamin Franklin", Surname = "Gates", DateOfBirth = GenerateRandomBirthday(), Email = "bfgates@example.com", IsActive = true },
+            new User { Id = 3, Forename = "Castor", Surname = "Troy", DateOfBirth = GenerateRandomBirthday(), Email = "ctroy@example.com", IsActive = false },
+            new User { Id = 4, Forename = "Memphis", Surname = "Raines", DateOfBirth = GenerateRandomBirthday(), Email = "mraines@example.com", IsActive = true },
+            new User { Id = 5, Forename = "Stanley", Surname = "Goodspeed", DateOfBirth = GenerateRandomBirthday(), Email = "sgodspeed@example.com", IsActive = true },
+            new User { Id = 6, Forename = "H.I.", Surname = "McDunnough", DateOfBirth = GenerateRandomBirthday(), Email = "himcdunnough@example.com", IsActive = true },
+            new User { Id = 7, Forename = "Cameron", Surname = "Poe", DateOfBirth = GenerateRandomBirthday(), Email = "cpoe@example.com", IsActive = false },
+            new User { Id = 8, Forename = "Edward", Surname = "Malus", DateOfBirth = GenerateRandomBirthday(), Email = "emalus@example.com", IsActive = false },
+            new User { Id = 9, Forename = "Damon", Surname = "Macready", DateOfBirth = GenerateRandomBirthday(), Email = "dmacready@example.com", IsActive = false },
+            new User { Id = 10, Forename = "Johnny", Surname = "Blaze", DateOfBirth = GenerateRandomBirthday(), Email = "jblaze@example.com", IsActive = true },
+            new User { Id = 11, Forename = "Robin", Surname = "Feld", DateOfBirth = GenerateRandomBirthday(), Email = "rfeld@example.com", IsActive = true },
         });
 
     public DbSet<User>? Users { get; set; }
