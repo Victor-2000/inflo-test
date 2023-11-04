c2e7acc94fcc2befbc59a07055fb40eb4ecbd213
Victor <17victor.c@gmail.com>
2023-11-04 05:51:20 +0000

    Created log service

---
+++ UserManagement.Services/Implementations/LogService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UserManagement.Data;
+using UserManagement.Models;
+using UserManagement.Services.Domain.Interfaces;
+
+namespace UserManagement.Services.Domain.Implementations;
+
+public class LogService : ILogsService
+{
+    private readonly IDataContext _dataAccess;
+    public LogService(IDataContext dataAccess) => _dataAccess = dataAccess;
+
+    public IEnumerable<Log> GetAll() => _dataAccess.GetAll<Log>();
+
+    public IEnumerable<Log> FindLogsbyUserId(long userId) => _dataAccess.GetAll<Log>().Where(_dataAccess => _dataAccess.Id == userId);
+}
---
+++ UserManagement.Services/Interfaces/ILogsService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UserManagement.Models;
+
+namespace UserManagement.Services.Domain.Interfaces;
+
+public interface ILogsService
+{
+    public IEnumerable<Log> GetAll();
+
+    public IEnumerable<Log> FindLogsbyUserId(long userId);
+}
