5443ab55fba52f69a56433473675ec439c2fae05
Victor <17victor.c@gmail.com>
2023-11-04 05:49:53 +0000

    Created log models

---
+++ UserManagement.Data/Entities/Log.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserManagement.Models;
+
+public class Log
+{
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
+    public long UserId { get; set; } = default!;
+    public List<string> PropertyNames { get; set; } = new List<string>();
+    public List<string> PropertyOldValues { get; set; } = new List<string>();
+    public List<string> PropertyNewValues { get; set; } = new List<string>();
+    public LogType Type { get; set; } = default!;
+    public DateTime DateTimeOfIssue { get; set; } = default!;
+}
