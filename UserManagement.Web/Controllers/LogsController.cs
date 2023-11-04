80b2d748ce898d7772f140d2a78023453decf0d1
Victor <17victor.c@gmail.com>
2023-11-04 05:52:44 +0000

    Set up logs controller

---
+++ UserManagement.Web/Controllers/LogsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using UserManagement.Services.Domain.Interfaces;
+
+namespace UserManagement.Web.Controllers;
+public class LogsController : Controller
+{
+
+    private readonly ILogsService _logsService;
+    public LogsController(ILogsService logsService) => _logsService = logsService;
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+}
